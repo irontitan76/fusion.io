61ebb992099f9d907161d7e26fdc0e901edb98f3
Ross Sheppard <ross_sheppard@outlook.com>
2018-12-18 18:03:04 -0600

    Updated Landing Page for non-existent organizations

--- client/src/views/Landing/index.js
+++ client/src/views/Landing/index.js
@@ -15,6 +15,10 @@ const styles = theme => ({
     paddingBottom: theme.spacing.unit * 7,
     paddingTop: theme.spacing.unit * 7,
   },
+  infoContainer: {
+    height: '100vh',
+    paddingTop: theme.spacing.unit * 5,
+  }
 });
 
 class Landing extends Component {
@@ -24,35 +28,43 @@ class Landing extends Component {
     const locationMap = {
       '/ai': {
         theme: theme.palette.ai,
-        name: 'Fusion A.I.'
+        name: 'Fusion A.I.',
+        info: 'Fusion A.I. is the 3rd phase in the Fusion Strategy.',
       },
       '/cosmos': {
         theme: theme.palette.cosmos,
-        name: 'Fusion Cosmos'
+        name: 'Fusion Cosmos',
+        info: 'Fusion Cosmos is the 10th phase in the Fusion Strategy.',
       },
       '/energy': {
         theme: theme.palette.energy,
-        name: 'Fusion Energy'
+        name: 'Fusion Energy',
+        info: 'Fusion Energy is the 8th phase in the Fusion Strategy',
       },
       '/finance': {
         theme: theme.palette.finance,
-        name: 'Fusion Finance'
+        name: 'Fusion Finance',
+        info: 'Fusion Finance is the 5th phase in the Fusion Strategy.',
       },
       '/health': {
         theme: theme.palette.health,
-        name: 'Fusion Health'
+        name: 'Fusion Health',
+        info: 'Fusion Health is the 7th phase in the Fusion Strategy.',
       },
       '/legal': {
         theme: theme.palette.legal,
-        name: 'Fusion Legal'
+        name: 'Fusion Legal',
+        info: 'Fusion Legal is the 6th phase in the Fusion Strategy.',
       },
       '/media': {
         theme: theme.palette.media,
-        name: 'Fusion Media'
+        name: 'Fusion Media',
+        info: 'Fusion Media is the 4th phase in the Fusion Strategy.',
       },
       '/transport': {
         theme: theme.palette.transport,
-        name: 'Fusion Transport'
+        name: 'Fusion Transport',
+        info: 'Fusion Transport is the 9th phase in the Fusion Strategy.',
       },
     };
 
@@ -71,6 +83,19 @@ class Landing extends Component {
               className={classes.heading}
               variant='h2'>
               {org.name}
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid container justify='center'>
+          <Grid
+            className={classes.infoContainer}
+            item
+            xs={12}>
+            <Typography
+              align='center'
+              className={classes.info}
+              variant='subtitle1'>
+              {org.info}
             </Typography>
           </Grid>
         </Grid>
